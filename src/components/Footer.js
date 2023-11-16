d11afc960819d47f22fc174d6fa6b43d92335174
Drew <dev.drewragadio@gmail.com>
2023-11-16 13:39:55 -0500

    footer: comment links section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,9 @@ const Footer = () => {
     return (
         <footer className='w-full bg- bottom-0 absolute bg-[#285430] bg-gradient-to-tr from-emerald-800 to-sky-700 dark:from-emerald-800 dark:to-zinc-800'>
             <div className='divide-y-2 divide-stone-500 px-5'>
-                <div className='text-center py-10 text-sm text-green-100'>
+                {/* <div className='text-center py-10 text-sm text-green-100'>
                     Footer Links 
-                </div>
+                </div> */}
                 <div className='text-center py-3 text-xs text-neutral-300'>
                     © { Math.round(new Date().getUTCFullYear())} All Rights Reserved.
                 </div>
