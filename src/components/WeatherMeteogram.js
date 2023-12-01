64e5f410e8dd4769439d8e7f8526333223969c4d
Drew <dev.drewragadio@gmail.com>
2023-12-01 02:00:23 -0500

    current condition ui: skeleton enhancement

--- src/components/WeatherMeteogram.js
+++ src/components/WeatherMeteogram.js
@@ -132,15 +132,14 @@ const WeatherMeteogram = ({ cityWeather }) => {
                         </div>
                         <div role="status">
                             <div className="flex justify-end">
-                                <div className='w-3/4'>
-                                    <div className="h-7 w-1/2 bg-gray-200 rounded-full dark:bg-gray-700 mb-1"></div>
-                                    <div className="h-2 w-48 bg-gray-200 rounded-full dark:bg-gray-700  mb-1"></div>
-                                    <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-1"></div>
-                                    <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-4"></div>
-                                    
-                                    <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-1"></div>
+                                <div className='w-1/2'>
+                                    <div className="h-7 w-3/4 bg-gray-200 rounded-full dark:bg-gray-700 mt-2"></div>
+                                    <div className="h-2 w-3/4 bg-gray-200 rounded-full dark:bg-gray-700  mt-2"></div>
+                                    <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mt-2"></div>
                                 </div>
-                                
+                            </div>  
+                            <div className='flex justify-end'>
+                                <div className="h-2 w-1/2 bg-gray-200 rounded-full dark:bg-gray-700 mt-4"></div>
                             </div>
                         </div>
                     </div>  
