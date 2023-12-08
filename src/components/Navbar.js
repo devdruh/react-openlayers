4d34d28c41bbed0974cf605b19e666cc42968666
Drew <dev.drewragadio@gmail.com>
2023-12-08 17:17:20 -0500

    ui enhancement: dark mode config

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,6 +5,7 @@ const Navbar = () => {
 
     const { i18n } = useTranslation();
     const [lang, setLang] = useState();
+    const [isDarkTheme, setIsDartkTheme] = useState(false);
 
     const handleChangeLanguage = (e) => {
         const newLang = e.target.innerText === 'English' ? 'en' : 'fr';
@@ -12,13 +13,35 @@ const Navbar = () => {
         setLang(newLang);
     };
 
+    const handleChangeTheme = (e) => {
+        setIsDartkTheme(!isDarkTheme);
+        document.documentElement.classList.toggle('dark');
+        if (localStorage.getItem('color-theme')) {
+            if (localStorage.getItem('color-theme') === 'light') {
+                localStorage.setItem('color-theme', 'dark');
+            } else {
+                localStorage.setItem('color-theme', 'light');
+            }
+        }
+    }
+
     useEffect(() => {
         setLang(i18n.language)
+
+        if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('color-theme', 'dark');
+            setIsDartkTheme(true);
+        } else {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('color-theme', 'light');
+            setIsDartkTheme(false);
+        }
     }, [i18n]);
 
     return (
         <>
-            <nav className="bg-gradient-to-tr from-gray-50 to-sky-200 border-gray-200 dark:bg-gray-900 dark:border-gray-700 z-20 relative shadow-md">
+            <nav className="bg-gradient-to-tr from-gray-50 to-sky-200 border-gray-200 dark:bg-gray-900 dark:bg-gradient-to-br dark:from-gray-950 dark:to-gray-800 z-20 relative shadow-md">
                 <div className="flex flex-wrap justify-between items-center mx-auto p-4">
                     <a href="/" className="flex items-center">
                         {/* <img src='https://via.placeholder.com/120/120&text=LOGO' className='h-8 mr-3 rounded-full' alt='Logo here' /> */}
@@ -28,8 +51,21 @@ const Navbar = () => {
                         <span className="ml-2 text-slate-900 self-center text-2xl font-semibold whitespace-nowrap dark:text-white">LOGO</span>
                     </a>
                     <div className="flex items-center md:order-2 space-x-1 md:space-x-0 rtl:space-x-reverse">
-                        <button type="button" className="inline-flex items-center font-medium justify-center px-4 py-2 text-sm text-gray-900 dark:text-white rounded-lg cursor-pointer dark:hover:text-white" onClick={handleChangeLanguage}>
+                        <button type="button" className="inline-flex items-center font-medium justify-center px-4 py-2 text-sm text-slate-600 dark:text-white rounded-lg cursor-pointer dark:hover:text-white" onClick={handleChangeLanguage}>
                             {lang === 'en' ? 'Français' : 'English'}
+                        </button>
+                        <button type="button" className="text-slate-600 hover:scale-110 hover:ease-in hover:duration-300 hover:bg-slate-100 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-200 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:hover:text-white dark:focus:ring-slate-800 dark:hover:bg-slate-700" onClick={handleChangeTheme}>
+                            {
+                                isDarkTheme ? 
+                                    <svg id='light-theme' className="w-4 h-4 text-slate-700 hover:text-slate-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                        <path d="M10 15a5 5 0 1 0 0-10 5 5 0 0 0 0 10Zm0-11a1 1 0 0 0 1-1V1a1 1 0 0 0-2 0v2a1 1 0 0 0 1 1Zm0 12a1 1 0 0 0-1 1v2a1 1 0 1 0 2 0v-2a1 1 0 0 0-1-1ZM4.343 5.757a1 1 0 0 0 1.414-1.414L4.343 2.929a1 1 0 0 0-1.414 1.414l1.414 1.414Zm11.314 8.486a1 1 0 0 0-1.414 1.414l1.414 1.414a1 1 0 0 0 1.414-1.414l-1.414-1.414ZM4 10a1 1 0 0 0-1-1H1a1 1 0 0 0 0 2h2a1 1 0 0 0 1-1Zm15-1h-2a1 1 0 1 0 0 2h2a1 1 0 0 0 0-2ZM4.343 14.243l-1.414 1.414a1 1 0 1 0 1.414 1.414l1.414-1.414a1 1 0 0 0-1.414-1.414ZM14.95 6.05a1 1 0 0 0 .707-.293l1.414-1.414a1 1 0 1 0-1.414-1.414l-1.414 1.414a1 1 0 0 0 .707 1.707Z"/>
+                                    </svg>
+                                    :
+                                    <svg id='dark-theme' className="w-4 h-4 text-slate-700 hover:text-slate-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
+                                        <path d="M17.8 13.75a1 1 0 0 0-.859-.5A7.488 7.488 0 0 1 10.52 2a1 1 0 0 0 0-.969A1.035 1.035 0 0 0 9.687.5h-.113a9.5 9.5 0 1 0 8.222 14.247 1 1 0 0 0 .004-.997Z"/>
+                                    </svg>
+                            }
+                            <span className="sr-only">Light/Dark theme</span>
                         </button>
                     </div>
                 </div>
--- src/layout/Index.js
+++ src/layout/Index.js
@@ -4,11 +4,11 @@ import Footer from '../components/Footer'
 
 const Index = () => {
     return (
-        <>
+        <div className="dark:bg-slate-900">
             <Navbar />
             <Outlet/>
             <Footer />
-        </>
+        </div>
     )
 }
 
