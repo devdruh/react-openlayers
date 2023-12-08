e3842315572bffdd65767fa0d1fe3d391150cd5d
Drew <dev.drewragadio@gmail.com>
2023-12-08 00:42:57 -0500

    ui enhancement: footer position

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,13 +2,14 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <footer className='w-full bg- bottom-0 sm:absolute bg-[#285430] bg-gradient-to-tr from-emerald-800 to-sky-700 dark:from-emerald-800 dark:to-zinc-800'>
+        // md:bottom-0 md:absolute
+        <footer className='w-full translate-y-full relative bg-[#285430] bg-gradient-to-tr from-emerald-800 to-sky-700 dark:from-emerald-800 dark:to-zinc-800'>
             <div className='divide-y-2 divide-stone-500 px-5'>
                 {/* <div className='text-center py-10 text-sm text-green-100'>
                     Footer Links 
                 </div> */}
                 <div className='text-center py-3 text-xs text-neutral-300'>
-                    © { Math.round(new Date().getUTCFullYear())} All Rights Reserved.
+                    © { Math.round(new Date().getUTCFullYear())} All Rights Reserved. Dev - <a href='https://github.com/devdruh'>Andrew Ragadio</a>
                 </div>
             </div>
         </footer>
