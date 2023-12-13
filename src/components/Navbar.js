bbd6f1bd076107d4d269066d36312d2ea488e9c2
Drew <dev.drewragadio@gmail.com>
2023-12-13 16:52:51 -0500

    ui enhancement: navbar - chart dom catch

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -41,12 +41,16 @@ const Navbar = () => {
         if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
             document.documentElement.classList.add('dark');
             localStorage.setItem('color-theme', 'dark');
-            forecastWeatherChart.firstChild.classList.add('highcharts-dark');
+            if (forecastWeatherChart !== null) {
+                forecastWeatherChart.firstChild.classList.add('highcharts-dark');
+            }
             setIsDartkTheme(true);
         } else {
             document.documentElement.classList.remove('dark');
             localStorage.setItem('color-theme', 'light');
-            forecastWeatherChart.firstChild.classList.add('highcharts-light');
+            if (forecastWeatherChart !== null) {
+                forecastWeatherChart.firstChild.classList.add('highcharts-light');
+            }
             setIsDartkTheme(false);
         }
     }, [i18n]);
