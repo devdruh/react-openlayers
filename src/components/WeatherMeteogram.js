98c8499b330e815636758708b5357919143d0327
Drew <dev.drewragadio@gmail.com>
2023-12-12 20:56:33 -0500

    ui enhancement: forecast icon pos

--- src/components/WeatherMeteogram.js
+++ src/components/WeatherMeteogram.js
@@ -202,17 +202,25 @@ const WeatherMeteogram = ({ cityWeather }) => {
                                     <div className='flex sm:flex-row justify-center '>
                                         {
                                             currentCondition?.iconCode ?
-                                                <img src={`https://meteo.gc.ca/weathericons/${currentCondition?.iconCode}.gif`} className='translate-y-1' width={60} height={51} alt={`${currentCondition?.condition}`} /> : null
+                                                <>
+                                                    <div className='sm:hidden'>
+                                                        <img src={`https://meteo.gc.ca/weathericons/${currentCondition?.iconCode}.gif`} className='max-sm:translate-y-2 translate-x-2' width={60} height={51} alt={`${currentCondition?.condition}`} />
+                                                    </div>
+                                                    <div className='max-sm:hidden'>
+                                                        <img src={`https://meteo.gc.ca/weathericons/${currentCondition?.iconCode}.gif`} className='translate-y-1 translate-x-2' width={60} height={51} alt={`${currentCondition?.condition}`} />
+                                                    </div>
+                                                </>
+                                                : null
                                         }
                                         <p className="text-xl md:text-5xl font-semibold ml-2 md:ml-5 text-gray-900 dark:text-slate-300">{currentCondition?.temperature ? Math.floor(currentCondition?.temperature) : null}</p>
                                         {/* <p className='pl-2 flex gap-1'><a href='/'>°C</a> | <a href='/' className='text-gray-500'>°F</a></p> */}
-                                        <p className='pl-1 flex gap-1 text-gray-500 dark:text-gray-400'>°C</p>
+                                        <p className='pl-1 flex gap-1 text-slate-600 dark:text-gray-400'>°C</p>
                                         <div className='text-xs pl-4 whitespace-nowrap'>
-                                            <p className='font-normal text-gray-500 dark:text-gray-400'><span className='max-sm:dark:font-medium'>{t('common:dewPoint')}:</span> {currentCondition?.dewpoint}°C </p>
-                                            <p className='font-normal text-gray-500 dark:text-gray-400'><span className='max-sm:dark:font-medium'>{t('common:humidity')}:</span> {currentCondition?.relativeHumidity}%</p>
+                                            <p className='font-normal text-slate-600 dark:text-gray-400'><span className='max-sm:dark:font-medium'>{t('common:dewPoint')}:</span> {currentCondition?.dewpoint}°C </p>
+                                            <p className='font-normal text-slate-600 dark:text-gray-400'><span className='max-sm:dark:font-medium'>{t('common:humidity')}:</span> {currentCondition?.relativeHumidity}%</p>
                                             {
                                                 currentCondition?.wind ?
-                                                    <p className='font-normal text-gray-500 dark:text-gray-400'><span className='max-sm:dark:font-medium'>{t('common:wind')}:</span> {
+                                                    <p className='font-normal text-slate-600 dark:text-gray-400'><span className='max-sm:dark:font-medium'>{t('common:wind')}:</span> {
                                                         currentCondition?.wind.direction + ' ' + currentCondition?.wind.speed
                                                     }
                                                         {
@@ -222,7 +230,7 @@ const WeatherMeteogram = ({ cityWeather }) => {
                                                     null
                                             }
                                             {
-                                                currentCondition?.windChill !== undefined ? <p className='font-normal text-gray-500 dark:text-gray-400'><span className='max-sm:dark:font-medium'>{t('common:windChill')}:</span> {currentCondition?.windChill}</p> : null
+                                                currentCondition?.windChill !== undefined ? <p className='font-normal text-slate-600 dark:text-gray-400'><span className='max-sm:dark:font-medium'>{t('common:windChill')}:</span> {currentCondition?.windChill}</p> : null
                                             }
 
                                         </div>
@@ -230,15 +238,15 @@ const WeatherMeteogram = ({ cityWeather }) => {
                                 </div>
                                 <div>
                                     <div className='text-right'>
-                                        <p className='text-sm md:text-lg max-sm:font-medium text-gray-500 dark:text-gray-400'>{t('common:weather')}</p>
-                                        <p className='text-xs md:text-sm font-thin dark:font-normal text-gray-500 dark:text-gray-400'>{new Date(currentCondition?.dateTime[1].year, currentCondition?.dateTime[1].month - 1, currentCondition?.dateTime[1].day, currentCondition?.dateTime[1].hour, currentCondition?.dateTime[1].minute).toLocaleDateString(i18n.language === 'fr' ? 'fr-CA' : undefined, { weekday: 'short', hour: 'numeric', minute: 'numeric', timeZoneName: 'short' })}</p>
-                                        <p className='text-xs md:text-sm font-thin dark:font-normal text-gray-500 dark:text-gray-400'>{currentCondition?.condition}</p>
+                                        <p className='text-sm md:text-lg max-sm:font-medium text-slate-600 dark:text-gray-400'>{t('common:weather')}</p>
+                                        <p className='text-xs md:text-sm dark:font-normal text-slate-600 dark:text-gray-400'>{new Date(currentCondition?.dateTime[1].year, currentCondition?.dateTime[1].month - 1, currentCondition?.dateTime[1].day, currentCondition?.dateTime[1].hour, currentCondition?.dateTime[1].minute).toLocaleDateString(i18n.language === 'fr' ? 'fr-CA' : undefined, { weekday: 'short', hour: 'numeric', minute: 'numeric', timeZoneName: 'short' })}</p>
+                                        <p className='text-xs md:text-sm dark:font-normal text-slate-600 dark:text-gray-400'>{currentCondition?.condition}</p>
                                     </div>
                                 </div>
                                 <div className='col-span-2 columns-2 align-middle'>
-                                    <div className='pt-2'><p className='text-xs font-normal text-gray-400 dark:text-gray-400'><span className='max-sm:dark:font-medium'>{t('common:observedAt')}:</span> {currentCondition?.station}</p></div>
+                                    <div className='pt-2'><p className='text-xs font-normal text-slate-600 dark:text-gray-400'><span className='max-sm:dark:font-medium'>{t('common:observedAt')}:</span> {currentCondition?.station}</p></div>
 
-                                    <div className='pt-2 max-sm:pt-4 text-right'><p className='flex justify-end text-xs font-normal text-gray-400 dark:text-gray-400'>
+                                    <div className='pt-2 max-sm:pt-4 text-right'><p className='flex justify-end text-xs font-normal text-slate-600 dark:text-gray-400'>
                                         <svg className="w-5 h-5 text-slate-700 hover:text-slate-800 dark:text-slate-300" aria-hidden="true" fill="currentColor" xmlns="http://www.w3.org/2000/svg" version="1" viewBox="0 0 100 100">
                                             <path d="M415 910c-62-63-77-82-67-92s25 0 72 47l59 59 3-170c3-142 5-169 18-169s15 27 18 169l3 170 59-59c47-47 62-57 72-47s-5 29-67 92c-44 44-82 80-85 80s-41-36-85-80zM484 427c-3-8-4-43-2-78 2-49 7-64 18-64 12 0 15 15 15 74 0 51-4 76-12 78-7 3-15-2-19-10zM200 307c0-16 91-107 108-107 24 0 11 27-36 73-48 47-72 58-72 34zM727 272c-47-48-58-72-34-72 16 0 107 91 107 108 0 24-27 11-73-36zM412 194c-56-28-88-65-103-119l-10-34-147-3C29 35 5 33 5 20S67 5 500 5s495 2 495 15-24 15-147 18l-147 3-12 37c-19 58-45 88-101 116-66 33-110 33-176 0zm176-41c31-23 58-60 67-90l6-23H500c-144 0-161 2-156 16 3 9 6 19 6 24 0 17 55 73 85 86 40 19 120 12 153-13z"
                                                 transform="matrix(.1 0 0 -.1 0 100)"></path>
